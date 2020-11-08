30cac554b603f17814e7ee55b83d72fda5ef2cca
pc <pc@DESKTOP-JD1I3FP>
2020-11-08 17:12:11 +0000

    8/11

--- PharmacyApi/Migrations/20201108170116_bvttt.Designer.cs
+++ PharmacyApi/Migrations/20201108170116_bvttt.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PharmacyApi.Authentication;
 
 namespace PharmacyApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201108170116_bvttt")]
+    partial class bvttt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- PharmacyApi/Models/Supplier.cs
+++ PharmacyApi/Models/Supplier.cs
@@ -10,7 +10,7 @@ namespace PharmacyApi.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 
-        public int ID { get; set; }
+        public int? ID { get; set; }
         public string Name { get; set; }
         public string ContactPerson { get; set; }
         public string City { get; set; }
